6899c2dbca5ba9c2dc0f48bc08e0d0fce23888cc
Ashish Bairagi <abbackend98@gmail.com>
2024-01-23 19:20:51 +0530

    Added unique validation

--- app/Http/Requests/CourseRequest.php
+++ app/Http/Requests/CourseRequest.php
@@ -15,11 +15,17 @@ class CourseRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+        $rules = [
             'name' => 'required|string',
-            'code' => 'required|string',
+            'code' => 'required|string|unique:courses,code',
             'duration' => 'required|integer',
             'duration_type' => Rule::enum(CourseType::class)
         ];
+
+        if ($this->isMethod('PUT') && $course = $this->route('course')) {
+            $rules['code'] .= ", {$course->id}";
+        }
+
+        return $rules;
     }
 }
